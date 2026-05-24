57e62b14b118ef0dc9a004bc4d2c75dffb751e8c
Akesh Indudunu <153709228+akeshindudunu@users.noreply.github.com>
2026-05-24 22:27:18 +0530

    fix:img fixed

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   output: "export",  // <=== enables static exports
   basePath: "/Portfolio",
   assetPrefix: "/Portfolio",
+  images: {
+    unoptimized: true,       // Required: GitHub Pages does not support native Next.js image optimization
+  },
 };
 
 export default nextConfig;
