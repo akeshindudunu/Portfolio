1f0a0c8f4085a9a03f1495f567188b33a77d06f2
Akesh Indudunu <153709228+akeshindudunu@users.noreply.github.com>
2026-05-24 20:15:19 +0530

    fix: add assetPrefix for GitHub Pages basePath

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/Portfolio",
+  assetPrefix: "/Portfolio",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
