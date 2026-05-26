8a3ecc5ebce8d2730b510b0566701dcd074bc094
Akesh Indudunu <153709228+akeshindudunu@users.noreply.github.com>
2026-05-26 16:32:30 +0530

    fix:assetPrefix was corrected

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",  // <=== enables static exports
-  basePath: "/Portfolio",
-  assetPrefix: "/Portfolio"
+ output: 'export', // Required for GitHub Pages static hosting
+  basePath: '/Portfolio', // Required for GitHub Pages static hosting
+  assetPrefix: '/Portfolio/',// Required for GitHub Pages static hosting
+  images: {
+    unoptimized: true, // Required for static export if using next/image
+  },
 };
 
 export default nextConfig;
